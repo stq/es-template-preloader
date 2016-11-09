a530f0e952bcd069a717c95a850d8ffbfcc01ba6
viktar.kachan <v.kachan@andersenfsoft.ru>
2016-11-09 09:08:33 +0300

    Fixing stuff.

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,17 +1,38 @@
 module.exports = {
     resolve: {
         root: './',
-        modules: [ 'test', 'node_modules', './']
+        modulesDirectories: ['test', 'node_modules', './']
     },
     entry: {
         main: 'source'
     },
-    module : {
+    output: {
+        path: './test/out',
+        publicPath: '',
+        filename: '[name].js'
+    },
+    module: {
         preLoaders: [
             {
                 test: /\.js$/,
                 loader: 'es-template-preloader'
             }
+        ],
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: [/node_modules/, /bower_components/, /datagrid/]
+        }, {
+            test: /\.html$/,
+            loader: 'raw' + (production && '!html-minify' || ''),
+            exclude: /node_modules/
+        }
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('source', 'source.js', Infinity)
+    ]
+
+
 };
